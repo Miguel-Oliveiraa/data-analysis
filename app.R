571c5a72a2e4196f2c1e80046621d91761d0749d
miguel-oliveiraa <miguellgomes64@gmail.com>
2023-09-14 15:55:19 -0300

    new dataset and dates filter

--- app.R
+++ app.R
@@ -6,7 +6,7 @@ library(ggplot2)
 library(dplyr)
 
 # Carregue seus dados
-valve <- read.csv("Valve_Player_Data.csv")
+apple_data <- read.csv("AAPL.csv")
 
 ui <- dashboardPage(
   dashboardHeader(title = "Dashboard"),
@@ -25,38 +25,32 @@ ui <- dashboardPage(
                   selectInput(
                     inputId = "classe",
                     label = "Escolha uma classe",
-                    choices = c("Average players", "Peak Players"),
+                    choices = c("Preço abertura", "Preço Maximo", "Preço Minimo", "Preço fechamento", "Preço ajustado", "Volume"),
                     selected = NULL,
                   ),
                   dateInput(
                     inputId="start_date_1",
                     label="Selecione a data inicial",
-                    value = NULL,
-                    min = "mm-yyyy",
-                    max = "mm-yyyy",
-                    format = "mm-yyyy",
+                    value = "1980-12-12",
+                    min = "1980-12-12",
+                    max = "2022-03-24",
+                    format = "yyyy-mm-dd",
                     startview = "year",
                     weekstart = 0,
                     language = "pt-br",
-                    width = NULL,
                     autoclose = TRUE,
-                    datesdisabled = NULL,
-                    daysofweekdisabled = NULL
                   ),
                   dateInput(
                     inputId="end_date_1",
                     label="Selecione a data final",
-                    value = NULL,
-                    min = NULL,
-                    max = NULL,
-                    format = "mm-yyyy",
+                    value = "2022-03-24",
+                    min = "1980-12-12",
+                    max = "2022-03-24",
+                    format = "yyyy-mm-dd",
                     startview = "year",
                     weekstart = 0,
                     language = "pt-br",
-                    width = NULL,
                     autoclose = TRUE,
-                    datesdisabled = NULL,
-                    daysofweekdisabled = NULL
                   ),
                 ),
                 mainPanel(
@@ -80,22 +74,22 @@ ui <- dashboardPage(
                   selectInput(
                     inputId = "classe_x",
                     label = "Eixo x",
-                    choices = c("Average players", "Peak Players"),
+                    choices = c("Preço abertura", "Preço Maximo", "Preço Minimo", "Preço fechamento", "Preço ajustado", "Volume"),
                     selected = NULL,
                   ),
                   selectInput(
                     inputId = "classe_y",
                     label = "Eixo y",
-                    choices = c("Average players", "Peak Players"),
+                    choices = c("Preço abertura", "Preço Maximo", "Preço Minimo", "Preço fechamento", "Preço ajustado", "Volume"),
                     selected = NULL,
                   ),
                   dateInput(
-                    inputId="start_date",
+                    inputId="start_date_2",
                     label="Selecione a data inicial",
-                    value = NULL,
-                    min = "mm-yyyy",
-                    max = "mm-yyyy",
-                    format = "mm-yyyy",
+                    value = "1980-12-12",
+                    min = "1980-12-12",
+                    max = "2022-03-24",
+                    format = "yyyy-mm-dd",
                     startview = "year",
                     weekstart = 0,
                     language = "pt-br",
@@ -105,12 +99,12 @@ ui <- dashboardPage(
                     daysofweekdisabled = NULL
                   ),
                   dateInput(
-                    inputId="end_date",
+                    inputId="end_date_2",
                     label="Selecione a data final",
-                    value = NULL,
-                    min = NULL,
-                    max = NULL,
-                    format = "mm-yyyy",
+                    value = "2022-03-24",
+                    min = "1980-12-12",
+                    max = "2022-03-24",
+                    format = "yyyy-mm-dd",
                     startview = "year",
                     weekstart = 0,
                     language = "pt-br",
@@ -142,12 +136,24 @@ server <- function(input, output) {
   # Crie uma função reativa para mapear a opção selecionada no selectInput para o nome da coluna correspondente
   selected_column <- reactive({
     switch(input$classe,
-           "Average players" = "Avg_players",
-           "Peak Players" = "Peak_Players"
+           "Preço abertura" = "Open",
+           "Preço Maximo" = "High",
+           "Preço Minimo" = "Low",
+           "Preço fechamento"= "Close",
+           "Preço ajustado" = "Adj_Close",
+           "Volume" = "Volume"
     )
   })
   
+  # Coletar as datas de início e fim selecionadas pelos usuários
+  start_date_1 <- reactive({as.Date(input$start_date_1)}) 
+  end_date_1 <- reactive({as.Date(input$end_date_1)})
   
+  # Filtre seus dados com base nas datas selecionadas
+  filtered_data_1 <- reactive({
+    apple_data %>%
+      filter(Date >= start_date_1() & Date <= end_date_1())
+  })
   ############################ Tabela aba 01 #######################
   #calcula moda
   calculate_mode <- function(x) {
@@ -157,11 +163,9 @@ server <- function(input, output) {
   }
   
   # Crie um resumo com base na coluna selecionada
-  valve_summary <- reactive({
+  apple_summary <- reactive({
     col_name <- selected_column()
-    
-    summary_data <- valve %>%
-      group_by(Game_Name) %>%
+    summary_data <- filtered_data_1() %>%
       summarise(
         moda = calculate_mode(get(col_name)),
         média = mean(get(col_name)),
@@ -174,27 +178,43 @@ server <- function(input, output) {
     return(summary_data)
   })
   # Renderize a tabela com base nos dados resumidos
-  output$table_01 <- renderTable(valve_summary())
+  output$table_01 <- renderTable(apple_summary())
   
   ######################## ABA 02 ##################################
   selected_column_x <- reactive({
     switch(input$classe_x,
-           "Average players" = "Avg_players",
-           "Peak Players" = "Peak_Players"
+           "Preço abertura" = "Open",
+           "Preço Maximo" = "High",
+           "Preço Minimo" = "Low",
+           "Preço fechamento"= "Close",
+           "Preço ajustado" = "Adj_Close",
+           "Volume" = "Volume"
     )
   })
   selected_column_y <- reactive({
     switch(input$classe_y,
-           "Average players" = "Avg_players",
-           "Peak Players" = "Peak_Players"
+           "Preço abertura" = "Open",
+           "Preço Maximo" = "High",
+           "Preço Minimo" = "Low",
+           "Preço fechamento"= "Close",
+           "Preço ajustado" = "Adj_Close",
+           "Volume" = "Volume"
     )
+  })
+  
+  # Coletar as datas de início e fim selecionadas pelos usuários
+  start_date_2 <- reactive({as.Date(input$start_date_2)}) 
+  end_date_2 <- reactive({as.Date(input$end_date_2)})
+  
+  filtered_data_2 <- reactive({
+    apple_data %>%
+      filter(Date >= start_date_2() & Date <= end_date_2())
   })
   
   ############################ Tabela aba 02 #######################
   # Crie um resumo com base na coluna selecionada
-  correlacao_por_jogo <- reactive({
-    valve %>%
-      group_by(Game_Name) %>%
+  correlacao_dados <- reactive({
+    filtered_data_2() %>%
       summarise(
         Correlacao = cor(.data[[selected_column_x()]], .data[[selected_column_y()]], method = "spearman")
       )
@@ -202,7 +222,7 @@ server <- function(input, output) {
   
   # Renderize a tabela de correlação por jogo
   output$table_02 <- renderTable({
-    correlacao_por_jogo()
+    correlacao_dados()
   })
   
 }
